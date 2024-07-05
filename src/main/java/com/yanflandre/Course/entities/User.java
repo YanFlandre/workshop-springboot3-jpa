5c744c224b0d25c0fdd4858328e6acb5d5668876
YanFlandre <jp_felippe@hotmail.com>
2024-07-05 19:57:38 -0300

    Order, Instant part2

--- src/main/java/com/yanflandre/Course/entities/User.java
+++ src/main/java/com/yanflandre/Course/entities/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,6 +30,7 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 
@@ -42,6 +45,7 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+
 	}
 
 	public Long getId() {
---
+++ src/main/java/com/yanflandre/Course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.yanflandre.Course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yanflandre.Course.entities.Order;
+import com.yanflandre.Course.entities.User;
+import com.yanflandre.Course.repositories.OrderRepository;
+
+@Service
+
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
