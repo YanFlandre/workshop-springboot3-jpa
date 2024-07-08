b59ac98f4416acd68e58f3f5a2ddd7c4afc171e2
YanFlandre <jp_felippe@hotmail.com>
2024-07-08 14:12:42 -0300

    Subtotal and total methods

--- src/main/java/com/yanflandre/Course/entities/OrderItem.java
+++ src/main/java/com/yanflandre/Course/entities/OrderItem.java
@@ -20,18 +20,18 @@ public class OrderItem implements Serializable {
 	@EmbeddedId
 	private OrderItemPk id = new OrderItemPk();
 
-	private Integer quatity;
+	private Integer quantity;
 	private Double price;
 
 	public OrderItem() {
 
 	}
 
-	public OrderItem(Order order, Product product, Integer quatity, Double price) {
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
 		id.setOrder(order);
 		id.setProduct(product);
-		this.quatity = quatity;
+		this.quantity = quantity;
 		this.price = price;
 	}
 
@@ -54,12 +54,12 @@ public class OrderItem implements Serializable {
 		id.setOrder(order);
 	}
 
-	public Integer getQuatity() {
-		return quatity;
+	public Integer getQuantity() {
+		return quantity;
 	}
 
-	public void setQuatity(Integer quatity) {
-		this.quatity = quatity;
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
 	}
 
 	public Double getPrice() {
@@ -68,6 +68,10 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	public Double getSubTotal() {
+		return price * quantity;
 	}
 
 	@Override
