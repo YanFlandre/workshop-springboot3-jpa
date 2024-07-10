b114d2084c6a9b540ea06a6931495209da4a0368
YanFlandre <jp_felippe@hotmail.com>
2024-07-10 10:13:08 -0300

    Exception Handler

--- src/main/java/com/yanflandre/Course/services/UserService.java
+++ src/main/java/com/yanflandre/Course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.yanflandre.Course.entities.User;
 import com.yanflandre.Course.repositories.UserRepository;
+import com.yanflandre.Course.services.exceptions.ResourceNotFoundException;
 
 @Service
 
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
